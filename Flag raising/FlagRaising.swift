4552b3db82816b904d352552baedf75960b7bd84
Sanjith7146 <sanjith7146@gmail.com>
2021-06-16 16:22:19 +0800

    Flag raising: Added Reset Flag button + Alert for it
    Heightened Alert:
    Changed UI - Fonts
    Added alerts for going back and forwards
    Now able to  go back
    Made the moving amount larger

--- Flag raising/FlagRaising.swift
+++ Flag raising/FlagRaising.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct FlagRaising: View {
     
     @State var flag: CGFloat = 0
+    @State var resetFlag = false
     
     var body: some View {
         ZStack {
@@ -24,22 +25,48 @@ struct FlagRaising: View {
                         .padding()
                         .offset(x: -30, y: flag)
                         .frame(maxHeight: 100, alignment: .center)
-                    
+                
                 }
               
-                Button("Majulah! ") {
-                    withAnimation {
-                        flag -= 10
+                HStack {
+                    Button("Majulah!") {
+                        withAnimation {
+                            flag -= 10
+                        }
                     }
+                    .padding()
+                    .background(Color(red: 0.6901960784313725, green: 0.7686274509803922, blue: 0.6941176470588235))
+                    .foregroundColor(Color(red: 0.5882352941176471, green: 0.027450980392156862, blue: 0.0))
+                    .font(.system(size: 20))
+                    .cornerRadius(20)
+                    .padding()
+                    
+                    Button("Reset"){
+                        withAnimation{
+                            resetFlag = true
+                        }
+                    }
+                    .padding()
+                    .background(Color(red: 0.6901960784313725, green: 0.7686274509803922, blue: 0.6941176470588235))
+                    .foregroundColor(/*@START_MENU_TOKEN@*/Color(red: 0.5882352941176471, green: 0.40784313725490196, blue: 0.44313725490196076)/*@END_MENU_TOKEN@*/)
+                    .font(.system(size: 20))
+                    .cornerRadius(20)
+                    .padding()
+                    .alert(isPresented: $resetFlag){
+                        Alert(title: Text("Are you sure you want to reset the flag?"),
+                              primaryButton: .default(Text("Yes, reset")){
+                                withAnimation{
+                                   flag = 0
+                                }
+                              },
+                              secondaryButton: .default(Text("No")){
+                                resetFlag = false
+                           })
+                    }
+                    
                 }
-                .padding()
-                .background(Color(hue: 0.139, saturation: 1.0, brightness: 1.0))
-                .foregroundColor(Color(red: 0.5882352941176471, green: 0.027450980392156862, blue: 0.0))
-                .font(.system(size: 20))
-                .cornerRadius(20)
-                .padding()
                 
-                
+              //Spacer()
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
             .padding()
--- Flag raising/HeightenedAlert.swift
+++ Flag raising/HeightenedAlert.swift
@@ -10,12 +10,14 @@ import SwiftUI
 struct HeightenedAlert: View {
     
     @State var alert: CGFloat = 0
+    @State var leftHouse = false
     
     var body: some View {
         ZStack {
             Color(red: 0.871, green: 0.859, blue: 0.824).ignoresSafeArea()
             VStack {
-                Text("Alert!")
+                Text("Heightened Alert")
+                    .fontWeight(.heavy)
                     .padding()
                     .background(Color(red: 0.6901960784313725, green: 0.7686274509803922, blue: 0.6941176470588235))
                     .foregroundColor(/*@START_MENU_TOKEN@*/Color(red: 0.5882352941176471, green: 0.40784313725490196, blue: 0.44313725490196076)/*@END_MENU_TOKEN@*/)
@@ -27,11 +29,12 @@ struct HeightenedAlert: View {
                 
                 Button("🦠 Heighten 🦠") {
                     withAnimation {
-                        alert -= 10
+                      //  alert -= 10
+                        leftHouse = true
                     }
                 }
                 .padding()
-                .background(Color(hue: 0.139, saturation: 1.0, brightness: 1.0))
+                .background(Color(hue: 0.139, saturation: 1.0, brightness: 1.0)) 
                 .foregroundColor(Color(red: 0.5882352941176471, green: 0.027450980392156862, blue: 0.0))
                 .font(.system(size: 20))
                 .cornerRadius(20)
@@ -40,12 +43,22 @@ struct HeightenedAlert: View {
             .frame(maxHeight: .infinity,
                    alignment: .bottom)
             .padding()
+            .alert(isPresented: $leftHouse){
+                Alert(title: Text("Why you want more covid?"),
+                      message: Text("Later got CB again, you get killed by everyone"),
+                      primaryButton: .default(Text("Huh, sorry lah, i just playing, i got back now")){
+                        withAnimation {
+                            alert += 20
+                        }
+                      },
+                      secondaryButton: .default(Text("Idc, i trained in martial arts, come try to kill me.")){
+                        withAnimation {
+                            alert -= 20
+                        }
+                      })
+            }
         }
-        
-        
-        
     }
-    
 }
 
 struct HeightenedAlert_Previews: PreviewProvider {
